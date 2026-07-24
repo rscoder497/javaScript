f145329a0f855a6f48a04161347c631d9465702a
Lalit Tiwari <lalitofficial497@gmail.com>
2026-07-24 11:11:23 +0530

    Create 001_datatypes.js

---
+++ mainjavascript/001_datatypes.js
@@ -0,0 +1,40 @@
+// Variable in js 
+// _firstName ✅
+// name$hello ✅
+// hello ✅
+// hello3 ✅
+// ❌  3hell0
+
+// Variable are used to store data 
+
+// Data types in Js 
+
+// Data Types in js 
+// Premetive and Non-premitive
+
+// Premitive data types are already assigned
+// Types of Premitive Data Types
+// Number ==> bignt , int ,decimal
+// String ==> "Harry"
+// undefined
+// null
+
+let a = 23.3434;
+
+// a = "shaurya";
+
+console.log(typeof a);
+
+// let name = prompt("hello enter your name ");
+
+// console.log(name);
+
+
+let ka = null;
+
+console.log(typeof ka);
+
+
+
+
+
