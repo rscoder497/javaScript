60dcf98a00fe9701204f3ed1120af65d6d3d0b9e
Lalit Tiwari <lalitofficial497@gmail.com>
2026-07-24 12:06:07 +0530

    Create 003_Naming_of_variable_in_js.js

---
+++ mainjavascript/003_Naming_of_variable_in_js.js
@@ -0,0 +1,38 @@
+// Rules for Naming Variables
+
+// you can't start with number
+
+// 1value (invalid ❌)
+
+//  value1 (valid ✅)
+
+var value1 = 10;
+
+var value2 = 2;
+
+console.log(value1 / value2 );
+
+console.log(value1 ** value2);
+
+
+
+
+// You can use Underscore( _ ) and Doller ( $ ) Symbol 
+// _firstName (valid ✅)
+
+// first$Name (valid ✅)
+
+var first_Name = "Shaurya";
+var first$Name = "Divyanshu";
+
+ console.log(first_Name);
+ console.log(first$Name);
+
+
+// You can't use Spaces in naming variables
+// first name  (invalid ❌)
+
+// convenction ⁉️
+// Use start with small lettes use camelCase
+// ex ==> firstName
+
