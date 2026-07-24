c123fa1f791a97d4277927f596c48da1aa54db6d
Lalit Tiwari <lalitofficial497@gmail.com>
2026-07-24 12:10:48 +0530

    Create 004_let_keyword_in_js.js

---
+++ mainjavascript/004_let_keyword_in_js.js
@@ -0,0 +1,13 @@
+// let keyword 
+// declare variable with let keyword
+
+let firstName = "Shaurya";
+// var firstName = "Divyanshu";
+
+firstName = "Divyanshu";
+
+
+console.log(firstName);
+
+
+// block scope vs function scope cover later in this video 
