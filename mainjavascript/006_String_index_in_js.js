8eed6261574bd5430b52f00c891bce6a74437270
Lalit Tiwari <lalitofficial497@gmail.com>
2026-07-24 12:26:56 +0530

    Create 006_String_index_in_js.js

---
+++ mainjavascript/006_String_index_in_js.js
@@ -0,0 +1,13 @@
+// String Indexing
+
+let firstName = "Shaurya";
+
+// S  H  A  U  R  Y  A
+// 0  1  2  3  4  5  6
+
+console.log(firstName[6]);
+
+console.log(firstName[firstName.length-1]);
+
+
+
