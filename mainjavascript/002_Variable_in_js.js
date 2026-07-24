1cfb8669d5335b086d5f4a3206e77bcd7a28776f
Lalit Tiwari <lalitofficial497@gmail.com>
2026-07-24 11:47:05 +0530

    Update 002_Variable_in_js.js

--- mainjavascript/002_Variable_in_js.js
+++ mainjavascript/002_Variable_in_js.js
@@ -1,1 +1,19 @@
 
+// intro to Variables 
+
+// Variables Can strore some information in it.
+
+
+// Declaration of Variable
+
+var firstName = "Lalit";
+
+// we can use that information later 
+
+console.log(firstName);
+
+// we can change that information 
+
+firstName = "Shaurya";
+
+console.log(firstName);
